d220c3c89f734a22afc8aafa933dd14984470e01
Zach Weintraub <zachweintraub@gmail.com>
2020-12-19 19:59:39 -0800

    build

--- public/app.js
+++ public/app.js
@@ -71044,7 +71044,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59601" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55631" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
